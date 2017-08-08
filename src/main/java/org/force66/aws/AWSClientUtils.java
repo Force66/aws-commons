2bae8cf91e1df7951b26da67f7f09debd6e03243
Derek-Ashmore <dashmore@force66.com>
2017-08-08 06:17:37 -0400

    Interim check-in

--- src/main/java/org/force66/aws/AWSClientUtils.java
+++ src/main/java/org/force66/aws/AWSClientUtils.java
@@ -1,10 +1,19 @@
 package org.force66.aws;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.exception.ContextedRuntimeException;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
+import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
 
 /**
  * Collection of one-line utilities to get configured AWS clients
@@ -18,7 +27,7 @@ public class AWSClientUtils {
 	 * @return AmazonS3 S3Client
 	 */
 	public static AmazonS3 buildAmazonS3() {
-		return AmazonS3ClientBuilder.defaultClient();
+		return buildAmazonS3(null, null);
 	}
 	
 	/**
@@ -27,11 +36,34 @@ public class AWSClientUtils {
 	 * @return AmazonS3 S3Client
 	 */
 	public static AmazonS3 buildAmazonS3(String regionName) {
-		Regions regions = validateRegionName(regionName);
+		return buildAmazonS3(regionName, null);
+	}
+	
+	/**
+	 * Builds a standard S3 client specifying the region provided under an assumed role.
+	 * @param regionName
+	 * @param assumedRoleRequest
+	 * @return AmazonS3 S3Client
+	 */
+	public static AmazonS3 buildAmazonS3(String regionName, AssumeRoleRequest assumedRole) {
+				
+		AmazonS3ClientBuilder s3Builder = null;
+		try {
+			s3Builder = AmazonS3ClientBuilder.standard();
+		} catch (Exception e) {
+			throw new ContextedRuntimeException("Error creating AWS S3 client", e);
+		}
 		
-		return AmazonS3ClientBuilder.standard()
-				.withRegion(regions)
-				.build();
+		if (StringUtils.isNotBlank(regionName)) {
+			Regions regions = validateRegionName(regionName);
+			s3Builder = s3Builder.withRegion(regions);
+		}
+		if (assumedRole != null) {
+			AWSCredentialsProvider creds = createAssumedRoleCredentials(assumedRole);
+			s3Builder = s3Builder.withCredentials(creds);
+		}
+		
+		return s3Builder.build();
 	}
 
 	protected static Regions validateRegionName(String regionName) {
@@ -39,6 +71,31 @@ public class AWSClientUtils {
 		Regions regions = Regions.fromName(regionName);
 		Validate.notNull(regions, "%s is not a valid region name", regionName);
 		return regions;
+	}
+	
+	protected static AWSCredentialsProvider createAssumedRoleCredentials(AssumeRoleRequest assumedRole) {
+		Validate.notNull("assumedRole can't be null");
+		AWSSecurityTokenServiceClientBuilder stsBuilder = null;
+		try {
+			stsBuilder = AWSSecurityTokenServiceClientBuilder.standard();
+		} catch (Exception e1) {
+			throw new ContextedRuntimeException("Error creating AWS STS client", e1);
+		}
+		
+		AssumeRoleResult requestResult = null;
+		try {
+			requestResult = stsBuilder.build().assumeRole(assumedRole);
+		} catch (Exception e) {
+			throw new ContextedRuntimeException("Error assuming AWS role", e)
+				.addContextValue("assumedRole", ReflectionToStringBuilder.toString(assumedRole));
+		}
+		
+		BasicSessionCredentials tempCreds = new BasicSessionCredentials(
+				requestResult.getCredentials().getAccessKeyId(), 
+				requestResult.getCredentials().getSecretAccessKey(), 
+				requestResult.getCredentials().getSessionToken());
+		
+		return new AWSStaticCredentialsProvider(tempCreds);
 	}
 
 }
--- src/test/java/org/force66/aws/AWSClientUtilsTestIntegration.java
+++ src/test/java/org/force66/aws/AWSClientUtilsTestIntegration.java
@@ -1,25 +1,37 @@
 package org.force66.aws;
 
+import org.apache.commons.lang3.exception.ContextedRuntimeException;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 
 public class AWSClientUtilsTestIntegration {
 
 	private static final String TEST_REGION = "us-east-1";
 
 	@Test
-	public void testbuildAmazonS3() {
+	public void testbuildAmazonS3Default() {
 		AmazonS3 s3 = AWSClientUtils.buildAmazonS3();
 		Assert.assertNotNull(s3);
 	}
 	
 	@Test
-	public void testBuildAmazonS3String() throws Exception {
+	public void testBuildAmazonS3WithRegion() throws Exception {
 		AmazonS3 s3 = AWSClientUtils.buildAmazonS3(TEST_REGION);
 		Assert.assertEquals(TEST_REGION, s3.getRegionName());
 	}
 
+	@Test(expected = ContextedRuntimeException.class)
+	public void testBuildAmazonS3WithRole() throws Exception {
+		AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest();
+		assumeRoleRequest.setDurationSeconds(900);
+		assumeRoleRequest.setRoleArn("arn:aws:iam::517214143524:role/S3-Admin-Access");
+		assumeRoleRequest.setRoleSessionName("demo");
+		
+		AmazonS3 s3 = AWSClientUtils.buildAmazonS3(TEST_REGION, assumeRoleRequest);
+		Assert.assertEquals(TEST_REGION, s3.getRegionName());
+	}
 
 }
