ed0e8f2fdc70e6e3ecdb13afe829840a959797c4
Derek-Ashmore <dashmore@force66.com>
2017-08-11 04:31:32 -0400

    Added AWSClientUtils unit tests

--- src/test/java/org/force66/aws/AWSClientUtilsTest.java
+++ src/test/java/org/force66/aws/AWSClientUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -22,6 +23,7 @@ import com.amazonaws.services.securitytoken.model.Credentials;
 @RunWith(MockitoJUnitRunner.class)
 public class AWSClientUtilsTest {
 	
+	private static final String TEST_REGION = "us-east-1";
 	private static final String TEST_SESSIONTOKEN = "testSessiontoken";
 	private static final String TEST_SECRET_ACCESS_KEY = "testSecretAccessKey";
 	private static final String TEST_ACCESS_KEY_ID = "testAccessKeyId";
@@ -83,7 +85,7 @@ public class AWSClientUtilsTest {
 	
 	@Test
 	public void testValidateValidRegionName() throws Exception {
-		Regions region = awsClientUtils.validateRegionName("us-east-1");
+		Regions region = awsClientUtils.validateRegionName(TEST_REGION);
 		Assert.assertEquals(region, Regions.US_EAST_1);
 	}
 	
@@ -98,6 +100,81 @@ public class AWSClientUtilsTest {
 		Assert.assertEquals(TEST_ACCESS_KEY_ID, credProvider.getCredentials().getAWSAccessKeyId());
 		Assert.assertEquals(TEST_SECRET_ACCESS_KEY, credProvider.getCredentials().getAWSSecretKey());
 		Assert.assertEquals(TEST_SESSIONTOKEN, ((BasicSessionCredentials)credProvider.getCredentials()).getSessionToken());
+	}
+	
+	@Test
+	public void testCreateAssumedRoleCredentials_StsException() throws Exception {
+		RuntimeException rExpt = new RuntimeException("crap");
+		Mockito.when(awsBuilderFactoryMock.createAWSSecurityTokenServiceClientBuilder())
+			.thenThrow(rExpt);
+		
+		try {
+			awsClientUtils.createAssumedRoleCredentials(assumeRoleRequest);
+			Assert.fail();
+		} catch(AWSCommonsException e) {
+			Assert.assertEquals(rExpt, e.getCause());
+			Assert.assertEquals(0, e.getContextEntries().size());
+		}
+	}
+	
+	@Test
+	public void testCreateAssumedRoleCredentials_AssumedRoleException() throws Exception {
+		RuntimeException rExpt = new RuntimeException("crap");
+		Mockito.when(awsSecurityTokenServiceMock.assumeRole(assumeRoleRequest))
+			.thenThrow(rExpt);
+		
+		try {
+			awsClientUtils.createAssumedRoleCredentials(assumeRoleRequest);
+			Assert.fail();
+		} catch(AWSCommonsException e) {
+			Assert.assertEquals(rExpt, e.getCause());
+			Assert.assertEquals(1, e.getContextEntries().size());
+		}
+	}
+	
+	@Test
+	public void testDefaultS3Create() throws Exception {
+		AmazonS3 s3 = awsClientUtils.buildAmazonS3();
+		Assert.assertEquals(amazonS3Mock, s3);
+	}
+	
+	@Test(expected = AWSCommonsException.class)
+	public void testS3CreateWithException() throws Exception {
+		Mockito.when(awsBuilderFactoryMock.createAmazonS3ClientBuilder())
+			.thenThrow(new RuntimeException("crap"));
+		awsClientUtils.buildAmazonS3();
+	}
+	
+	@Test
+	public void testS3CreateWithRegion() throws Exception {			
+		AmazonS3 s3 = awsClientUtils.buildAmazonS3(TEST_REGION);
+		Assert.assertEquals(amazonS3Mock, s3);
+		
+		validateRegion();
+	}
+
+	private void validateRegion() {
+		ArgumentCaptor<Regions> regionsArg = ArgumentCaptor.forClass(Regions.class);
+		Mockito.verify(amazonS3ClientBuilderProxyMock).withRegion(regionsArg.capture());
+		Assert.assertEquals(TEST_REGION, regionsArg.getValue().getName());
+	}
+	
+	@Test
+	public void testS3CreateWithRegionAndRole() throws Exception {
+		AmazonS3 s3 = awsClientUtils.buildAmazonS3(TEST_REGION, assumeRoleRequest);
+		Assert.assertEquals(amazonS3Mock, s3);
+		
+		validateRegion();
+		validateRole();
+	}
+
+	private void validateRole() {
+		ArgumentCaptor<AWSCredentialsProvider> credsArg = ArgumentCaptor.forClass(AWSCredentialsProvider.class);
+		Mockito.verify(amazonS3ClientBuilderProxyMock).withCredentials(credsArg.capture());
+		Assert.assertEquals(assumeRoleResult.getCredentials().getSecretAccessKey(), 
+				credsArg.getValue().getCredentials().getAWSSecretKey());
+		Assert.assertEquals(assumeRoleResult.getCredentials().getAccessKeyId(), 
+				credsArg.getValue().getCredentials().getAWSAccessKeyId());
 	}
 
 }
